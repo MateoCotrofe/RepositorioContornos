bdce6b81ccc45ca2d537795f52b4a849d08552c0
Programador B <progB@ejemplo.com>
2026-04-24 20:43:18 +0200

    Funcionalidad: Resta

--- Proyecto_calculadora/src/Calculadora.java
+++ Proyecto_calculadora/src/Calculadora.java
@@ -1,5 +1,5 @@
 public class Calculadora {
     public static void main(String[] args) {
-        System.out.println("--- Calculadora Iniciada ---");
+        System.out.println("Resta: " + Resta.operar(10, 5));
     }
 }
---
+++ Proyecto_calculadora/src/Resta.java
@@ -0,0 +1,3 @@
+public class Resta {
+    public static double operar(double a, double b) { return a - b; }
+}
