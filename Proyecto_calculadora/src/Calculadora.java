716d4992f59ac43b21a0a97d69a52611e3bfd5fb
Programador A <progA@ejemplo.com>
2026-04-24 20:55:43 +0200

    Merge: Conflicto resuelto

--- Proyecto_calculadora/src/Calculadora.java
+++ Proyecto_calculadora/src/Calculadora.java
@@ -1,5 +1,7 @@
 public class Calculadora {
     public static void main(String[] args) {
+        System.out.println("Suma: " + Suma.operar(10, 5));
+        System.out.println("Resta: " + Resta.operar(10, 5));
         System.out.println("Multi: " + Multiplicacion.operar(10, 5));
     }
 }
